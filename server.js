2b25a1c691e935b320f257f34ace33b0042bec91
antonioledezma13-prog <antonioledezma13@gmail.com>
2026-04-27 13:03:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 // ── Middlewares ────────────────────────────────────────────
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.frontend_URL || 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
